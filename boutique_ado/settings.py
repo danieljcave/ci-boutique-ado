14e516110d6b8c6feb9c996693f2c4f9917ea54d
danieljcave <>
2024-01-15 17:17:41 +0000

    Added add to bag functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -68,9 +68,9 @@ LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
