38e05cb5fdaeb15c68032c67acebc0499f8025b4
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2024-01-16 03:21:35 +0000

    Added toasts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'bag',
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
