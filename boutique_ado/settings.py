46a7aa66e020ff520418fbb2e2610066ac28bb28
Dan Cave <danielcave99@gmail.com>
2024-01-21 02:33:10 +0000

    update media queries

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('SECRET_KEY', '')
 
-ALLOWED_HOSTS = ['127.0.0.1',
+ALLOWED_HOSTS = ['127.0.0.1', 
+                 '8000-danieljcave-ciboutiquea-ua56elezams.ws-eu107.gitpod.io', 
                  'ci-boutique-ado-dc-7a99c9d6843d.herokuapp.com']
 
 
